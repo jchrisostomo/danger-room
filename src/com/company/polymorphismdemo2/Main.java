8fb700e575ab779e282b84263cff420888fbaf02
João Chrisóstomo <joao.chrisostomo@gmail.com>
2022-02-19 21:57:10 -0300

    new demo

--- src/com/company/polymorphismdemo2/Main.java
+++ src/com/company/polymorphismdemo2/Main.java
@@ -1,11 +1,22 @@
 package com.company.polymorphismdemo2;
 
+import com.company.polymorphismdemo2.entities.ImportedProduct;
+import com.company.polymorphismdemo2.entities.Product;
+import com.company.polymorphismdemo2.entities.UsedProduct;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
+        Product product1 = new Product("Notebook", 1100.0);
+        Product product2 = new UsedProduct("Iphone", 400.00, sdf.parse("15/03/2017"));
+        Product product3 = new ImportedProduct("Tablet", 260.00, 20.00);
 
-        
-
-
+        System.out.println(product1.priceTag());
+        System.out.println(product2.priceTag());
+        System.out.println(product3.priceTag());
     }
 }
